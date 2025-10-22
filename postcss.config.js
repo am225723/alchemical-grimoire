5fadfcf3d852e7fe9926ae322a0abb51c5e5fe77
am225723 <puertaale@gmail.com>
2025-10-22 11:16:21 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ import autoprefixer from 'autoprefixer';
 
 export default {
   plugins: [
-    tailwindcss,
-    autoprefixer,
+    tailwindcss, // Handles Tailwind processing
+    autoprefixer, // Still useful for vendor prefixes
   ],
 };
